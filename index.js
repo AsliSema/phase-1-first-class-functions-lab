8a7eb5146c933c62044d2a4ed8cdad9994d8b072
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-03-23 12:18:59 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function(drivers){
+    return [drivers[0], drivers[1]]
+    //return drivers.slice(0, 2)
+}
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(drivers.length-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return (ride) => num*ride
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers)
+}
